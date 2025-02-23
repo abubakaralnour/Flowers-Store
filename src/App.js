dfcad86b04c0b24b04fed059737e92d5e2dc67c7
abubakar <abubakaralnour43@gmal.com>
2025-02-23 11:48:44 +0300

    larg ads

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import ContextMainbanner from './Context/ContextMainbanner';
 import Main from './Main/Main';
 import RootHead from './Head/RootHead';
 import Cookies from './Cookees/Cookies';
+import LargAds from './Cookees/LargAds/LargAds';
 
 function App() {
   return (
     <div className="App">
       <ContextMainbanner>
    <Router>
-   
+  
      <RootHead/>
      <Main/>
     
    </Router>
    </ContextMainbanner>
    <Cookies/>
+
+   <LargAds/>
+   
     </div>
   );
 }
---
+++ src/Cookees/LargAds/MediunAds.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MediunAds = () => {
+  return (
+    <div>Mediun.Ads</div>
+  )
+}
+
+export default MediunAds
